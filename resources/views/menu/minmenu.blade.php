72451f759ac67606511a2d9fb5f43508020939f1
pmoy27 <pablo.oyarzok@gmail.com>
2024-06-17 15:55:26 -0400

    justify

--- resources/views/menu/minmenu.blade.php
+++ resources/views/menu/minmenu.blade.php
@@ -248,8 +248,8 @@
                     </a>
                 </li>
                 <li>
-                    <a href="/datos/{{$procedimiento->id}}" class="flex gap-2 items-center p-2 rounded-lg text-black   group @if(request()->routeIs('formulario.datos')) bg-gray-300 @endif">
-                        <div class="flex items-center gap-2">
+                    <a href="/datos/{{$procedimiento->id}}" class="flex gap-2 justify-between items-center p-2 rounded-lg text-black   group @if(request()->routeIs('formulario.datos')) bg-gray-300 @endif">
+                        <div class="flex items-center">
                             <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-folder">
                                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                                 <path d="M5 4h4l3 3h7a2 2 0 0 1 2 2v8a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-11a2 2 0 0 1 2 -2" />
